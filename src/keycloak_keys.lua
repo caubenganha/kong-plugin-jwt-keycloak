29e58f1db9daa243c11f7bde6ba09b515d4d7ee1
caubenganha <phancuongv@gmail.com>
2022-12-23 18:21:56 +0700

    Update response

--- src/keycloak_keys.lua
+++ src/keycloak_keys.lua
@@ -70,7 +70,7 @@ local function get_request_token(url, scheme, port, token)
     -- print("firstName access: ", tostring(json_data["firstName"]))
     -- print("lastName access: ", tostring(json_data["lastName"]))
     -- print("email access: ", tostring(json_data["email"]))
-    return json.decode(table.concat(res_data)), nil
+    return res_data, nil
 end
 
 local function get_wellknown_endpoint(well_known_template, issuer)
