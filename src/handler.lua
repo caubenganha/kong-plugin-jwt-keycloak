d8123f900bf63d88915bd244fd45067b7a87f122
caubenganha <phancuongv@gmail.com>
2022-12-26 13:29:02 +0700

    Validate check groups and roles
    update code

--- src/handler.lua
+++ src/handler.lua
@@ -321,10 +321,22 @@ local function do_authentication(conf)
     -- Verify api access
     kong.log.debug("Verify api access")
     if ok then
-        ok, err = validate_role_access(conf.role_attributes_template, jwt.claims.realm_access.roles, token)
+        -- Check roles and groups in token
+        if jwt.claims.realm_access == nil then
+            kong.log.warn('Roles is not assigned to the user in access token')
+            return false, { status = 403, message = "Roles is not assigned to the user in access token"}
+        end
+
+        ok, err = validate_role_access(conf.role_attributes_template, jwt.claims, token)
         if err then
+
             kong.log.warn("validate_role_access"..err)
-            ok, err = validate_group_access(conf.group_attributes_template, jwt.claims.group_api_access, token)
+            if jwt.claims.group_api_access == nil or #jwt.claims.group_api_access == 0 then
+                kong.log.warn('Groups is not assigned to the user in access token')
+                return false, { status = 403, message = "Groups is not assigned to the user in access token"}
+            end
+
+            ok, err = validate_group_access(conf.group_attributes_template, jwt.claims, token)
             if err then
                 kong.log.warn("validate_group_access"..err)
                 return false, { status = 403, message = err }
