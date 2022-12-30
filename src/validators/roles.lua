69c14543bb64d367882c76bdd4e23e9073ed2564
caubenganha <phancuongv@gmail.com>
2022-12-30 15:05:12 +0700

    Update code

--- src/validators/roles.lua
+++ src/validators/roles.lua
@@ -91,7 +91,7 @@ local function validate_role_access(role_attributes_template, token_claims, toke
 
     -- Check api_access in user_role which match route
     kong.log.debug("Match api_access in roles")
-    local validate_api_access, api_error = match_api_access(role_attributes, route)
+    local validate_api_access, api_error = match_roles_access(role_attributes, route)
     if validate_api_access then
         return true
     else
@@ -100,7 +100,7 @@ local function validate_role_access(role_attributes_template, token_claims, toke
     end
 end
 
-local function validate_group_access(group_attributes_template, token_claims, token)
+local function validate_group_access(group_attributes_template, role_attributes_template, token_claims, token)
     local route = kong.router.get_route().name
     kong.log.debug('validate_group_access route name' .. route)
 
@@ -110,21 +110,26 @@ local function validate_group_access(group_attributes_template, token_claims, to
     end
 
     -- Get user groups (detail) from list group
-    local user_group = {}
-    local groups_in_token = token_claims.group_api_access
+    local user_groups = {}
+    local groups_in_token = token_claims.group_access
     kong.log.debug('get groups_cofiguration ')
     for _, group in pairs(groups_in_token) do
         -- kong.log.debug('curr_allowed_api 1 ' .. curr_role["name"])
         for _, value in ipairs(groups_cofiguration) do
             if value.path == group then
-                table.insert(user_group, value)
+                table.insert(user_groups, value)
             end
         end
     end
 
     -- Check api_access in user_role which match route
     kong.log.debug("Match api_access in groups")
-    local validate_api_access, api_error = match_api_access(user_group, route)
+    local keycloak_roles_configuration, err = get_data(role_attributes_template, token)
+    if err then
+        return nil, err
+    end
+
+    local validate_api_access, api_error = match_groups_access(user_groups, keycloak_roles_configuration, route)
     if validate_api_access then
         return true
     else
@@ -134,7 +139,51 @@ local function validate_group_access(group_attributes_template, token_claims, to
 
 end
 
-function match_api_access(user_group, route)
+function match_groups_access(user_groups, keycloak_roles_configuration, route)
+    kong.log.debug("match_groups_access")
+    if user_groups == nil or #user_groups == 0 then
+        return false, "Groups or roles of user is not configed in keycloak "
+    end
+    --Get realm roles name from user_groups (UUID type)
+    local roles_name = {}
+    for _, realm_role in pairs(user_groups) do
+        if realm_role ~= nil and #realm_role.realmRoles then
+            for index, value in ipairs(realm_role.realmRoles) do
+                kong.log.debug("realmRoles "..value)
+                table.insert(roles_name, value)
+            end
+        end
+    end
+    --Match roles_name with keycloak_roles_configuration
+    kong.log.debug("Match role details")
+    local role_details = {}
+    for _, role in pairs(roles_name) do
+        for _, role_config in pairs(keycloak_roles_configuration) do
+            kong.log.debug()
+            if role_config.name == role then
+                table.insert(role_details, role_config)
+            end
+        end
+    end
+
+
+    kong.log.debug("Match api_access")
+    for _, role in pairs(role_details) do
+        kong.log.debug('api_access.attributes.api_access size: ' .. #role.attributes)
+        if role.attributes ~= nil and role.attributes.api_access ~= nil and #role.attributes.api_access > 0 then
+            for _, api in pairs(json.decode(table.concat(role.attributes.api_access))) do
+                kong.log.debug('validate_group_access API: ' .. api)
+                if api == route then
+                    return true
+                end
+            end
+        end
+    end
+    return false, "You have no role permission or group permission on the api".. route
+end
+
+
+function match_roles_access(user_group, route)
 
     if user_group == nil or #user_group == 0 then
         return false, "Groups or roles of user is not configed in keycloak "
